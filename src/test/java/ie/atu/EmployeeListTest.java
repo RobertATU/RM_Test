31cf870c63020a55000ecd05f842d0f56de7da8f
G00395612 <g00395612@atu.ie>
2022-12-07 12:42:47 +0000

    Add employee add

--- src/test/java/ie/atu/EmployeeListTest.java
+++ src/test/java/ie/atu/EmployeeListTest.java
@@ -1,7 +1,39 @@
 package ie.atu;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeListTest {
 
+    EmployeeList emlist = new EmployeeList();
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void addEmployeeTest(){
+        emlist.addEmployee(new Employee("Nathan","098765432112"));
+        assertEquals(1,emlist.arraySize);
+
+
+    }
+
+    @Test
+    void addEmployeeTestFail(){
+        emlist.addEmployee(new Employee("Nathan","098765432112"));
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> emlist.addEmployee(new Employee("Nat","09865432112")));
+
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 }
